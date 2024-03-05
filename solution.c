4e7b43687c13ac97395ff26fa4bc44e67a5b5eeb
Brice Guichou <brguicho@2A7.42angouleme.fr>
2024-03-05 14:02:52 +0100

    try of best-move

--- solution.c
+++ solution.c
@@ -1,12 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   solution2.c                                        :+:      :+:    :+:   */
+/*   solution.c                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: brguicho <brguicho@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/04 10:31:37 by brguicho          #+#    #+#             */
-/*   Updated: 2024/03/04 10:31:57 by brguicho         ###   ########.fr       */
+/*   Updated: 2024/03/05 14:00:58 by brguicho         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -46,4 +46,32 @@ long long get_min(t_list *stack)
 		stack = stack->next;
 	}
 	return (min);
+}
+
+int	calculate_best_move(t_stack data)
+{
+	int best;
+	long long min;
+	int	move;
+	int lst_size;
+	int i;
+
+	best = INT_MAX;
+	lst_size = ft_lstsize(data.stack_b);
+	i = 0;
+	while (data.stack_b);
+	{
+		min = get_min(data.stack_a);
+		if (i > lst_size / 2)
+			move = (calculate_position(data.stack_b, get_target(data.stack_a, data.stack_b, min))
+				+ calculate_position(data.stack_a, get_target(data.stack_a, data.stack_b, min))) - lst_size + 1;
+		else
+			move = (calculate_position(data.stack_b, get_target(data.stack_a, data.stack_b, min))
+				+ calculate_position(data.stack_a, get_target(data.stack_a, data.stack_b, min))) - 1;
+		if (move < best)
+			best = move;
+		data.stack_b = data.stack_b->next;
+		i++;	
+	}
+	return (best);
 }
